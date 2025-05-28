69cebf8e0a8d14e0330872c41a4b602066e31a03
lhx <2122537936@qq.com>
2025-05-28 15:09:14 +0800

    基础变量和简单类型转换

--- pythonDemo/demo0528/demo1.py
+++ pythonDemo/demo0528/demo1.py
@@ -1,2 +1,17 @@
-print("hello python")
-print("hello python3")
+#通过内置函数type()可以获取变量的类型
+a=97
+print(type(a))
+b=9.09
+print(type(b))
+c="hello world"
+print(type(c))
+d=True
+print(type(d))
+
+#通过一些内置的函数来实现变量的类型转换
+print(float(a))#int ->float  可以指定进制
+print(int(b))#float ->int
+print(str(a))#int ->str   可以指定编码格式
+print(chr(a))#int ->chr
+print(ord('a'))#chr ->int  （将一个字符转成对于ascll码值）
+#使用变量来保存数据，变量有不同的类型，常用的类型有int、float、str和bool。在有需要的情况下，可以通过 Python 内置的函数对变量进行类型转换
